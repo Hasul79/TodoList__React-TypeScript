e364fc5d42347845955752d32c76bbf832de0445
Hasul79 <hatuk2@mail.ru>
2023-01-16 21:41:49 +0400

    add tsx

--- src/Components/TodoTask.tsx
+++ src/Components/TodoTask.tsx
@@ -1,19 +1,27 @@
 import React from "react";
-import {ITask} from "../Interfaces";
+import { ITask } from "../Interfaces";
 
 interface Props {
-   task: ITask;
+  task: ITask;
+  completeTask(taskNameToDelete: string): void;
 }
 
-const TodoTask = ({task}: Props) => {
+const TodoTask = ({ task, completeTask }: Props) => {
   return (
-  <div className="task">
-    {task.taskName} 
-    {task.deadline}
+    <div className="task">
+      <div className="content">
+        <span>{task.taskName}</span>
+        <span>{task.deadline}</span>
+      </div>
+      <button
+        onClick={() => {
+          completeTask(task.taskName);
+        }}
+      >
+        X
+      </button>
     </div>
-  )
-  
+  );
 };
-
 
 export default TodoTask;
